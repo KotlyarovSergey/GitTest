a6d4d8417564a13c28757ad6d4fa16f6cce4ab43
KotlyarovSergey <44573775+KotlyarovSergey@users.noreply.github.com>
2018-11-05 15:56:59 +0300

    Update Program.cs
    add decimal

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -27,6 +27,7 @@ namespace GitTest
             WriteLine("{0,-10} {1,-3} {2,28} {3,28}", "ulong", sizeof(ulong), ulong.MinValue.ToString(), ulong.MaxValue.ToString());
 
             WriteLine("{0,-10} {1,-3} {2,28} {3,28}", "double", sizeof(double), double.MinValue.ToString(), double.MaxValue.ToString());
+            WriteLine("{0,-10} {1,-3} {2,28} {3,28}", "decimal", sizeof(decimal), decimal.MinValue.ToString(), decimal.MaxValue.ToString());
             WriteLine();
         }
 
